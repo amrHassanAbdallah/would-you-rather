ff6486d73ff71b76dd1b72a08a78d1a3778f9f3c
Amr Hassan <amrh434@gmail.com>
2021-05-04 16:51:41 +0200

    feat : categorizing user's questions.

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,29 +2,49 @@ import React from 'react';
 import Question from "./question";
 import {connect} from "react-redux";
 import {Redirect} from "react-router-dom";
+import {Tabs} from "react-bootstrap";
+import Tab from "@material-ui/core/Tab";
 
 class Home extends React.Component{
     render() {
-        const {sortedQuestions,authedUser} = this.props
+        const {authedUser,categorizedQuestions} = this.props
         if (!authedUser){
             return <Redirect to='/login'/>
         }
         return (
+
+
            <div className="container col-md-8">
-                   <div>
-                       {sortedQuestions.map((question_id)=>(
-                           <Question key={question_id} id={question_id} />
-                       ))}
-                   </div>}
+               <Tabs defaultActiveKey={"Unanswered Questions".toLowerCase()} id="uncontrolled-tab-example">
+                   {Object.keys(categorizedQuestions).map((category)=>(
+                       <Tab eventKey={category.toLowerCase()} title={category}>
+                           {categorizedQuestions[category].map((question_id)=>(
+                               <Question key={question_id} id={question_id} />
+                           ))}
+                           {categorizedQuestions[category].length == 0 &&(
+                               <div className="text-center text-info">You answered all questions</div>
+                           )}
+                       </Tab>
+                   ))}
+
+               </Tabs>
            </div>
         );
     }
 }
-function mapStateToProps ({questions ,authedUser}) {
+function mapStateToProps ({users,questions ,authedUser}) {
+    let categorizedQuestions = {"Unanswered Questions":[],"Answered Questions":[]}
+   let sortedQuestions = Object.keys(questions).sort((a,b)=>questions[b].timestamp - questions[a].timestamp)
+
+    for(let i =0;i<sortedQuestions.length && authedUser;i++){
+        let key = Object.keys(users[authedUser].answers).indexOf(sortedQuestions[i]) > -1? "Answered Questions":"Unanswered Questions"
+        categorizedQuestions[key].push(sortedQuestions[i])
+    }
+
     return {
         authedUser,
         questions,
-        sortedQuestions:Object.keys(questions).sort((a,b)=>questions[b].timestamp - questions[a].timestamp)
+        categorizedQuestions:categorizedQuestions,
     }
 }
 
